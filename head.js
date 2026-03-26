31de8b0bb7614c5d3c175d8a3a7e1e8ae2ac097f
Pannoz <bebroggi@gmail.com>
2026-03-26 15:38:17 +0100

    Update head.js

--- head.js
+++ head.js
@@ -1,6 +1,17 @@
 (function() {
+  // Google Analytics 4 (gtag.js)
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-85LLNHZDE2';
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function() { dataLayer.push(arguments); };
+  gtag('js', new Date());
+  gtag('config', 'G-85LLNHZDE2');
+
   // Inject fox emoji favicon into <head>
-  const link = document.createElement('link');
+  var link = document.createElement('link');
   link.rel = 'icon';
   link.type = 'image/svg+xml';
   link.href = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🦊</text></svg>';
